4a6c6116b2c5c6ad89784b408336f821c523ff64
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-04-13 21:45:33 +0300

    fix:correct column names according to the task

--- Task2.sql
+++ Task2.sql
@@ -1,6 +1,6 @@
 SELECT
-    prod.name as "Продукт",
-    cat.name as "Категория"
+    prod.name as "Имя продукта",
+    cat.name as "Имя категории"
 FROM product prod
          LEFT JOIN product_category pc ON prod.id = pc.product_id
          LEFT JOIN category cat ON cat.id = pc.category_id;
